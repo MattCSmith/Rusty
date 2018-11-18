bb63d13155c3d62ca52122dd141e59768846081a
MattCSmith <mrmcchow@gmail.com>
2018-11-18 23:21:58 +0000

    init commit

---
+++ bot.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const config = require("./config.json");
+ 
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+ 
+client.on("message", (message) => {
+  if (message.content.startsWith("user")) {
+    console.log(message.author.username, message.author.discriminator)
+    console.log(message.author.avatarURL)
+    
+  }
+});
+ 
+client.login(config.token);
